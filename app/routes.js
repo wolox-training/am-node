e07298c853b067d594346796386faead7c67ae93
AndresNumen <andresmurgo@gmail.com>
2022-06-14 10:55:36 -0300

    add protect route get users

--- app/routes.js
+++ app/routes.js
@@ -7,18 +7,20 @@ const { validarCampos } = require('./middlewares/validar-campos');
 const { check } = require('express-validator');
 
 const  validation  = require('./middlewares/validation');
+const { validarJWT } = require('./middlewares/validar-jwt');
 
 exports.init = app => {
   app.get('/health', healthCheck);
   app.get('/api/joke', controller.methodGET);
 
   app.post('/api/create/user',validation,validarCampos, userController.methodPOST);
-  app.get('/api/users', userController.methodGET);
-
   app.post('/users/sessions',[
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'El password es obligatorio').not().isEmpty(),
     validarCampos
   ],loginController .methodPOST);
+
+  app.get('/api/users',validarJWT, userController.methodGET);
+
   // app.put('/endpoint/put/path', [], controller.methodPUT);
 };
