3724f57cc43f9a7d9a0f6eb76b235ebe80c24451
AndresNumen <andresmurgo@gmail.com>
2022-06-15 12:04:32 -0300

    add validation email is @wolox

--- app/routes.js
+++ app/routes.js
@@ -4,30 +4,24 @@ const { healthCheck } = require('./controllers/healthCheck');
 const { loginController } = require('./controllers/loginController');
 const { userController } = require('./controllers/userController');
 const { validarCampos } = require('./middlewares/validar-campos');
-const { check } = require('express-validator');
 
-const  validation  = require('./middlewares/validation');
+
+const  {validation}  = require('./middlewares/validation');
+const  {validationSignIn}  = require('./middlewares/validation');
 const { validarJWT } = require('./middlewares/validar-jwt');
 const { tieneRole } = require('./middlewares/validar-roles');
+const { request } = require('express');
 
 exports.init = app => {
   app.get('/health', healthCheck);
   app.get('/api/joke', controller.methodGET);
-
+  app.get('/api/users',validarJWT, userController.methodGET);
+  
   app.post('/api/create/user',validation,validarCampos, userController.methodPOST);
-  app.post('/users/sessions',[
-    check('email', 'El email es obligatorio').isEmail(),
-    check('password', 'El password es obligatorio').not().isEmpty(),
-    validarCampos
-  ],loginController .methodPOST);
+  app.post('/users/sessions',validationSignIn,validarCampos,loginController .methodPOST);
+  app.post('/admin/users',validarJWT,tieneRole('admin'), validationSignIn,validarCampos,loginController .methodAdminPOST);
 
-  app.post('/admin/users',[
-    check('email', 'El email es obligatorio').isEmail(),
-    check('password', 'El password es obligatorio').not().isEmpty(),
-    validarCampos
-  ],loginController .methodAdminPOST);
 
-  app.get('/api/users',validarJWT,tieneRole('admin'), userController.methodGET);
 
   // app.put('/endpoint/put/path', [], controller.methodPUT);
 };
